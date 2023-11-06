e6fcba9a78c8f56548953b2b61142d50840e36f4
“lcc” <chengcheng.li@51hgp.com>
2023-11-06 19:57:18 +0800

    feat: nextTick_2

--- examples/apiNextTIck/App.js
+++ examples/apiNextTIck/App.js
@@ -1,4 +1,4 @@
-import { h, ref } from '../../lib/demo-vue3-esm.js';
+import { h, ref, nextTick } from '../../lib/demo-vue3-esm.js';
 
 // 在component render执行的时候，能够拿到当前组件的element vnode tree，children element vnode，以及children component vnode
 // 至于children component的element vnode以及其children，要等到mountChildren时进行遍历patch，才能获取到
@@ -8,6 +8,9 @@ export default {
     const btn = h('button', {
       onClick: this.onClick
     }, '执行')
+
+    nextTick(() => {
+    })
     return h('div', {}, [
       btn,
       h('div', {}, `${this.count}`)
